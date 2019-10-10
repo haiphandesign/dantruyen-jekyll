09aeeb21b49b25e27c4584fb7d3958fbe1240db2
Hải Phan <hai@haiphan.design>
2019-10-11 00:07:28 +1100

    User Profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -84,14 +84,32 @@ $('.post-interaction-item-vote').click(function () {
 
 // GAME CARD MANAGER
 
+//// GAME CARD MANAGER HEIGHT CALC
 function gameCardManagerHeightCalc() {
-    var gameCardManagerHeight = $('.game-card-manager-tab .game-card-preview').height();
-    $('.game-card-manager-tab .game-card-manager-left').css('max-height', `${gameCardManagerHeight}px`);
+    var gameCardManagerHeight = $('.game-card-manager-tab.is-active .game-card-preview').height();
+    $('.game-card-manager-tab.is-active .game-card-manager-left').css('max-height', `${gameCardManagerHeight}px`);
 };
 
-// GAME CARD LIST
+//// GAME CARD MANAGER TABS
+$('.game-card-manager>.tabs>ul>li').click(function () {
+    var gameCardManagerTabIndex = $(this).index();
+    $('.game-card-manager>.tabs>ul>li').removeClass('is-active');
+    $(this).addClass('is-active');
+    $('.game-card-manager .game-card-manager-tab').removeClass('is-active');
+    $('.game-card-manager .game-card-manager-tab').eq(gameCardManagerTabIndex).addClass('is-active');
+    gameCardManagerHeightCalc();
+});
+
+//// GAME CARD LIST
 
 $('.game-card-list .game-card').click(function () {
-    $(this).parent().children('.game-card').removeClass('is-selected');
-    $(this).addClass('is-selected');
+    if ($(this).hasClass('is-selected')) {
+        $(this).removeClass('is-selected');
+        $('.game-card-preview-interactions').removeClass('is-active');
+    } else {
+        $(this).parent().children('.game-card').removeClass('is-selected');
+        $(this).addClass('is-selected');
+        $('.game-card-preview-interactions').addClass('is-active');
+    }
+    gameCardManagerHeightCalc();
 });
